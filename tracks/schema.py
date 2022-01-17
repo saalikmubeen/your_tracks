83c3ba0a722b6733ef3f780556f30dc52be0d34a
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-01-17 22:06:27 +0530

    implemented update and delete track mutations

--- tracks/schema.py
+++ tracks/schema.py
@@ -32,7 +32,52 @@ class CreateTrack(graphene.Mutation):
         
         track = Track.objects.create(title=title, description=description, url=url, posted_by=info.context.user)
         return CreateTrack(track=track)
+
+
+class UpdateTrack(graphene.Mutation):
+    class Arguments:
+        id = graphene.String(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        url = graphene.String()
+
+    track = graphene.Field(TrackType)  # return type of the mutation
+
+    @login_required
+    def mutate(root, info, id, title, description, url):
+        track = Track.objects.get(pk=id)
+        
+        if track.posted_by != info.context.user:
+            raise Exception('You are not authorized to perform this action')
+        
+        track.title = title
+        track.description = description
+        track.url = url
+        
+        track.save()
+        
+        return UpdateTrack(track=track)
+    
+
+class DeleteTrack(graphene.Mutation):
+    class Arguments:
+        track_id = graphene.String(required=True)
+
+    track_id = graphene.String()  # return type of the mutation
+
+    @login_required
+    def mutate(root, info, track_id):
+        track = Track.objects.get(pk=track_id)
+
+        if track.posted_by != info.context.user:
+            raise Exception('You are not authorized to perform this action')
+
+        track.delete()
+
+        return DeleteTrack(track_id=track_id)
     
     
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
+    update_track = UpdateTrack.Field()
+    delete_track = DeleteTrack.Field()
