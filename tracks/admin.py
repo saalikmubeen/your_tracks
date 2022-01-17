c8dd132228087b22eaba253c9d296454e5728c35
saalik123 <63722568+saalik123@users.noreply.github.com>
2022-01-17 22:22:43 +0530

    added Like model

--- tracks/admin.py
+++ tracks/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Track
+from .models import Track, Like
 
 # Register your models here.
 
 
 admin.site.register(Track)
+admin.site.register(Like)
--- tracks/models.py
+++ tracks/models.py
@@ -14,7 +14,23 @@ class Track(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     posted_by = models.ForeignKey(
         get_user_model(), on_delete=models.CASCADE)
-    
+    class Meta:
+        ordering = ['-created_at']
     
     def __str__(self):
         return self.title
+
+
+class Like(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+    liked_by = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE)
+    track = models.ForeignKey(
+        Track, on_delete=models.CASCADE, related_name="likes")
+    
+    class Meta:
+        unique_together = ('liked_by', 'track',)
+        
+    def __str__(self):
+        return f'{self.liked_by} likes {self.track}'
